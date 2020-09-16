50315ce0d8a8a8635095f0f6299c34b3b3d42368
jovannitjuandi <jovanni.tjuandi@gmail.com>
2020-09-16 21:08:53 +1000

    adding prototype to github

---
+++ redback prototype/script-testresposinveheader.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(window).scroll(function() {
+  var header = $(document).scrollTop();
+  var headerHeight = $(".header").outerHeight();
+  var firstSection = $(".main-container section:nth-of-type(1)").outerHeight();
+  if (header > headerHeight) {
+    $(".header").addClass("fixed");
+  } else {
+    $(".header").removeClass("fixed");
+  }
+  if (header > firstSection) {
+    $(".header").addClass("in-view");
+  } else {
+    $(".header").removeClass("in-view");
+  }
+});
